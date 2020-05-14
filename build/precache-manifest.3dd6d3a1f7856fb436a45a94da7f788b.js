f0418748c310e3abafa7c194467aaeae1c5406ff
unknown <zafarustad123@gmail.com>
2020-05-14 19:46:06 +0530

    build

---
+++ build/precache-manifest.3dd6d3a1f7856fb436a45a94da7f788b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfaf62d3d2d48044e4d91c3e0633043e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12e4b54af5cff2d3d23d",
+    "url": "/static/css/main.f358da1e.chunk.css"
+  },
+  {
+    "revision": "f5c9971d25e9bb5e3ab8",
+    "url": "/static/js/2.3b2d49b2.chunk.js"
+  },
+  {
+    "revision": "5b42f95a40596c09be2c9addc99919aa",
+    "url": "/static/js/2.3b2d49b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e4b54af5cff2d3d23d",
+    "url": "/static/js/main.ae68ba5a.chunk.js"
+  },
+  {
+    "revision": "d0d78366e3f96c03a76a",
+    "url": "/static/js/runtime-main.62258c37.js"
+  },
+  {
+    "revision": "03e375f8ceb6317de145555caad0b152",
+    "url": "/static/media/empty.03e375f8.png"
+  },
+  {
+    "revision": "a311f37ea1a7661d13f5dbee3b5fd4b3",
+    "url": "/static/media/login2.a311f37e.png"
+  },
+  {
+    "revision": "b92341b317cd290558d2793bf7f5f21c",
+    "url": "/static/media/signup.b92341b3.png"
+  }
+]);
